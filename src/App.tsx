7df2c35c98d8d33f139a3b298be10b92fcd20ead
A. Shustovsky <a.shustovsky@gmail.com>
2023-08-11 11:46:54 +0400

    fix: cleaning up old files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-
 export function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/*<Route index element={<Home />} />*/}
-        {/*<Route path="about" element={<About />} />*/}
-        {/*<Route path="contact" element={<Contact />} />*/}
-        {/*<Route path="*" element={<NotFound />} />*/}
-      </Route>
-    </Routes>
-  );
+  return <></>;
 }
