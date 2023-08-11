f864a1aa56346e5bb0443bd5dd9fa3b1410f6bd0
A. Shustovsky <a.shustovsky@gmail.com>
2023-08-11 11:49:01 +0400

    fix: delete react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.tsx';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
